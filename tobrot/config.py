1909989855f88903c300f353a08bcd5c4ed28efa
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-29 23:21:02 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -12,3 +12,13 @@ class Config(Config):
     PYTDL_COMMAND_G = "pytdl"
     LEECH_COMMAND = "leech"
     INDEX_LINK = "https://toxik.suniljeyawardana.workers.dev/0:"
+    RCLONE_CONFIG = """
+[Kavindu aj]
+type = drive
+client_id = 689266221247-cks6608mg9oc4b6sdju953oah1o6cmve.apps.googleusercontent.com
+client_secret = lG1zuSUQs4KuEm6p0JnIJo5n
+scope = drive
+root_folder_id =
+token = {"access_token":"ya29.a0AfH6SMDN-6z_n-j3W7Ftkj_Xnp2BXZczmHyXQcfS7F8I3c7XxAUJwBjxv40mbfqKgpjLu-zTk0e0pzj60p-e8fbRGCmOUMniYPW1XqlXPZwabJBC3WyQic5r736lUtzkexVIbFYngHEgcFnsMNBUmtwz9A5ft_4mIkjJm0QfUwA","token_type":"Bearer","refresh_token":"1//0g695p0pmqnIOCgYIARAAGBASNwF-L9IrHu6FcCtrry52Dbl_aMFjZnWsKObLgKreDOCXjtt0LaKSjWqQP9DjDoB9nglmz3HseYI","expiry":"2021-01-29T05:56:56.0250148-08:00"}
+team_drive = 0AEnYW_mILPIFUk9PVA
+"""
