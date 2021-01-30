4e7ed3be6bb174af57a3c81f8227ba8e06342d16
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-29 22:40:02 -0800

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -8,7 +8,7 @@ class Config(Config):
     TG_BOT_TOKEN= "1436730490:AAFpVNY8ki0nzaLW8jotAQdca7bd3qAf2Zs"
     APP_ID = 1383845
     API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
-    OWNER_ID = 1156597097
+    OWNER_ID = 1131653685
     AUTH_CHANNEL = [-1001266398622]
     DESTINATION_FOLDER = "DOWNLOAD" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
