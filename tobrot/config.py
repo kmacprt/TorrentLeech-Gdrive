a0d9e0d66852f31fb8b75574e80aa0069c77caf3
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-29 22:01:36 -0800

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,23 @@
+class Config(Config):
+    TG_BOT_TOKEN= "1650920497:AAFSZ2slYjbl1gWyF_gqZwK5FMNv4oF6__8" #imp
+    APP_ID = 1733305 #imp
+    API_HASH = "f423cffca6b5b7247b31b5b0df61f48d" #imp
+    AUTH_CHANNEL = [-1001377338298, 1156597097] #imp replace your_id with your id from telegram or delete
+    GLEECH_COMMAND = "gleech"
+    YTDL_COMMAND = 'ytdl'
+    TELEGRAM_LEECH_COMMAND_G = "tleech@torlechlkbot"
+    CLONE_COMMAND_G = "gclone"
+    PYTDL_COMMAND_G = "pytdl"
+    DESTINATION_FOLDER = "Download"
+    LEECH_COMMAND = "leech"
+    INDEX_LINK = "https://toxik.suniljeyawardana.workers.dev/0:"
+    RCLONE_CONFIG = """
+[Kavindu aj]
+type = drive
+client_id = 689266221247-cks6608mg9oc4b6sdju953oah1o6cmve.apps.googleusercontent.com
+client_secret = lG1zuSUQs4KuEm6p0JnIJo5n
+scope = drive
+root_folder_id =
+token = {"access_token":"ya29.a0AfH6SMDN-6z_n-j3W7Ftkj_Xnp2BXZczmHyXQcfS7F8I3c7XxAUJwBjxv40mbfqKgpjLu-zTk0e0pzj60p-e8fbRGCmOUMniYPW1XqlXPZwabJBC3WyQic5r736lUtzkexVIbFYngHEgcFnsMNBUmtwz9A5ft_4mIkjJm0QfUwA","token_type":"Bearer","refresh_token":"1//0g695p0pmqnIOCgYIARAAGBASNwF-L9IrHu6FcCtrry52Dbl_aMFjZnWsKObLgKreDOCXjtt0LaKSjWqQP9DjDoB9nglmz3HseYI","expiry":"2021-01-29T05:56:56.0250148-08:00"}
+team_drive = 0AEnYW_mILPIFUk9PVA
+"""
