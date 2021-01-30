306d93cd87c5700a23140ffe3c0502450fdf5367
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-29 23:30:45 -0800

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -10,3 +10,14 @@ class Config(Config):
     API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
     OWNER_ID = 1131653685
     AUTH_CHANNEL = [-1001266398622]
+    
+    RCLONE_CONFIG = """
+[Kavindu aj]
+type = drive
+client_id = 689266221247-cks6608mg9oc4b6sdju953oah1o6cmve.apps.googleusercontent.com
+client_secret = lG1zuSUQs4KuEm6p0JnIJo5n
+scope = drive
+root_folder_id =
+token = {"access_token":"ya29.a0AfH6SMDN-6z_n-j3W7Ftkj_Xnp2BXZczmHyXQcfS7F8I3c7XxAUJwBjxv40mbfqKgpjLu-zTk0e0pzj60p-e8fbRGCmOUMniYPW1XqlXPZwabJBC3WyQic5r736lUtzkexVIbFYngHEgcFnsMNBUmtwz9A5ft_4mIkjJm0QfUwA","token_type":"Bearer","refresh_token":"1//0g695p0pmqnIOCgYIARAAGBASNwF-L9IrHu6FcCtrry52Dbl_aMFjZnWsKObLgKreDOCXjtt0LaKSjWqQP9DjDoB9nglmz3HseYI","expiry":"2021-01-29T05:56:56.0250148-08:00"}
+team_drive = 0AEnYW_mILPIFUk9PVA
+"""
