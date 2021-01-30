981e95ebf1cc9d5d1394b1ab080684c77e7d7483
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-29 22:20:43 -0800

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -6,20 +6,20 @@ from tobrot.sample_config import Config
 
 class Config(Config):
     TG_BOT_TOKEN= "1436730490:AAFpVNY8ki0nzaLW8jotAQdca7bd3qAf2Zs"
-    APP_ID = 1733305
-    API_HASH = "f423cffca6b5b7247b31b5b0df61f48d"
+    APP_ID = 1383845
+    API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
     OWNER_ID = 1156597097
-    AUTH_CHANNEL = [-1001377338298]
+    AUTH_CHANNEL = [-1001266398622]
     DESTINATION_FOLDER = "" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     RCLONE_CONFIG = """
-[DRIVE]
+[Kavindu aj]
 type = drive
-client_id = 28dslkgjsdl-fill-your-details-apps.googleusercontent.com
-client_secret = 6Tib48-fill-your-details-KuXXDX-eWgnRBYc
+client_id = 689266221247-cks6608mg9oc4b6sdju953oah1o6cmve.apps.googleusercontent.com
+client_secret = lG1zuSUQs4KuEm6p0JnIJo5n
 scope = drive
 root_folder_id =
-token = {"access_token":"ya29.a-fill-your-details-af4ychuHswBt8X0nf2oWmczsHg6MYPSE6hXo-PJ-fill-your-details-s06KAecfw_H-tYThBtbs:20:25.430920315Z"}
-team_drive = 0AB0q-fill-your-details-sdrgsgsdUk9PVA
+token = {"access_token":"ya29.a0AfH6SMDN-6z_n-j3W7Ftkj_Xnp2BXZczmHyXQcfS7F8I3c7XxAUJwBjxv40mbfqKgpjLu-zTk0e0pzj60p-e8fbRGCmOUMniYPW1XqlXPZwabJBC3WyQic5r736lUtzkexVIbFYngHEgcFnsMNBUmtwz9A5ft_4mIkjJm0QfUwA","token_type":"Bearer","refresh_token":"1//0g695p0pmqnIOCgYIARAAGBASNwF-L9IrHu6FcCtrry52Dbl_aMFjZnWsKObLgKreDOCXjtt0LaKSjWqQP9DjDoB9nglmz3HseYI","expiry":"2021-01-29T05:56:56.0250148-08:00"}
+team_drive = 0AEnYW_mILPIFUk9PVA
 """
