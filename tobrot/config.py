41f784a924a3e136a54d63ac1f3d84004443ed30
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-29 22:39:47 -0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -4,10 +4,10 @@ class Config(Config):
     TG_BOT_TOKEN= "1436730490:AAFpVNY8ki0nzaLW8jotAQdca7bd3qAf2Zs" #imp
     APP_ID = 1383845 #imp
     API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c" #imp
-    AUTH_CHANNEL = [-1001377338298, 1156597097] #imp replace your_id with your id from telegram or delete
+    AUTH_CHANNEL = [-1001377338298, 1131653685] #imp replace your_id with your id from telegram or delete
     GLEECH_COMMAND = "gleech"
     YTDL_COMMAND = 'ytdl'
-    TELEGRAM_LEECH_COMMAND_G = "tleech@torlechlkbot"
+    TELEGRAM_LEECH_COMMAND_G = "tleech"
     CLONE_COMMAND_G = "gclone"
     PYTDL_COMMAND_G = "pytdl"
     DESTINATION_FOLDER = "Download"
