4b22c86e76e49af6d8846764fd4b816a156ca392
kmacprt <72274248+kmacprt@users.noreply.github.com>
2021-01-29 22:36:41 -0800

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -10,7 +10,7 @@ class Config(Config):
     API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
     OWNER_ID = 1156597097
     AUTH_CHANNEL = [-1001266398622]
-    DESTINATION_FOLDER = "" #Name of your folder read readme(not id of the folder)
+    DESTINATION_FOLDER = "DOWNLOAD" #Name of your folder read readme(not id of the folder)
     #Just don't fill RCLONE_CONFIG vars, insted copy your rclone.conf file in root directory
     #if your wanted to fill -- fill your rclone config like this(Your config may have some extra value or less. so Don't worry)
     RCLONE_CONFIG = """
